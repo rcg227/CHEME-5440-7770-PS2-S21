b5b032655ec3d520f6daba2f4a5a6181934cdd4e
rcg227 <rcg227@cornell.edu>
2021-03-10 13:25:30 -0500

    Update Problem.jl

--- src/Problem.jl
+++ src/Problem.jl
@@ -11,21 +11,24 @@ function generate_problem_dictionary(path_to_parameters_file::String)::Dict{Stri
         # setup the initial condition array -
         initial_condition_array = [
             0.0 ;   # 1 mRNA
-            0.0 ;   # TODO: gene concentration goes here -
-            0.0 ;   # 3 I = we'll fill this in the execute script 
+            5.0 ;   # gene concentration (nM)
+            0.0 ;   # 3 I = we'll fill this in the execute script #QN: WHAT DOES THIS MEAN?
         ]
 
 
-        # TODO: calculate the mRNA_degradation_constant 
-        # mRNA_degradation_constant = ....
+        # TODO: calculate the mRNA_degradation_constant
+        mRNA_degradation_constant = log(2)/mRNA_half_life_in_min
+
+        # calculate the elongation constant (1/s)
+        elongation_constant = transcription_elongation_rate/gene_length_in_nt    #ask about this! should it be transcript length instead?
 
         # TODO: VMAX for transcription -
-        # VMAX = ...
+        VMAX = elongation_constant*RNAPII_concentration
 
         # TODO: Stuff that I'm forgetting?
         # ...
 
-        # --- PUT STUFF INTO problem_dictionary ---- 
+        # --- PUT STUFF INTO problem_dictionary ----
         problem_dictionary["transcription_time_constant"] = toml_dictionary["transcription_time_constant"]
         problem_dictionary["transcription_saturation_constant"] = toml_dictionary["transcription_saturation_constant"]
         problem_dictionary["E1"] = toml_dictionary["energy_promoter_state_1"]
@@ -36,8 +39,10 @@ function generate_problem_dictionary(path_to_parameters_file::String)::Dict{Stri
         problem_dictionary["initial_condition_array"] = initial_condition_array
 
         # TODO: If we want to use a value later e.g., VMAX or mRNA_degradation_constant you need to put them in the Dictionary
-        # ...
-        
+        problem_dictionary["mRNA_degradation_constant"]=mRNA_degradation_constant
+        problem_dictionary["elongation_constant"]=elongation_constant
+        problem_dictionary["VMAX"]=VMAX
+
         # return -
         return problem_dictionary
     catch error
